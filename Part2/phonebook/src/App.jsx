ca4f7e16a16012d28bf827b604694e48276b860b
Lorenzo Marotta <lz.marotta@gmail.com>
2024-01-03 00:12:12 +0100

    2.10

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const DisplayNames = ({ persons, filter }) => {
+const DisplayNames = ({ persons, filter, setPers, setN, setP }) => {
   return persons
     .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
     .map(person => (
@@ -8,6 +8,57 @@ const DisplayNames = ({ persons, filter }) => {
     ));
 }
 
+const PersonForm = ({ persons, newN, newP, setPers, setN, setP }) => {
+
+  const newName = newN;
+  const newPhone = newP;
+
+  const addName = (ev) => {
+    ev.preventDefault();
+    let checkName = false;
+    for (const i of persons) {
+      if (i.name === newName) {
+        alert(`${newName} is already added to phonebook`);
+        checkName = true;
+        break;
+      }
+    };
+    if (checkName) return;
+    const idName = newName.split(" ").slice(0, 1);
+    const obj = {
+      name: newName,
+      phone: newPhone,
+      id: idName[0]
+    };
+    setPers(persons.concat(obj));
+    setN('');
+    setP('');
+  };
+
+  const handleName = (ev) => {
+    setN(ev.target.value);
+  };
+  const handlePhone = (ev) => {
+    setP(ev.target.value);
+  };
+
+  return <form onSubmit={addName}>
+    <div>
+      name: <input value={newName} onChange={handleName} />
+      <div>number: <input value={newPhone} onChange={handlePhone} /></div>
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+}
+
+const Filter =({setF}) => {
+  const handleFilter = (ev) => {
+    setF(ev.target.value);
+  };
+  return <div>filter shown with <input type="text" onChange={handleFilter}/></div>
+}
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', phone: '040-1234567' }
@@ -16,54 +67,17 @@ const App = () => {
   const [newPhone, setNewPhone] = useState('');
   const [newFilter, setNewFilter] = useState('');
 
-  const addName = (ev) => {
-    ev.preventDefault();
-    let checkName = false;
-    for(const i of persons){
-      if(i.name === newName) {
-        alert(`${newName} is already added to phonebook`);
-        checkName=true;
-        break;}
-    };
-    if(checkName) return;
-    const idName = newName.split(" ").slice(0,1)
-    const obj = {
-      name: newName,
-      phone: newPhone,
-      id: idName[0]
-    };
-    setPersons(persons.concat(obj));
-    setNewName('');
-    setNewPhone('');
-  };
 
-  const handleName = (ev) => {
-    setNewName(ev.target.value);
-  };
-  const handlePhone = (ev) => {
-    setNewPhone(ev.target.value);
-  };
-  const handleFilter = (ev) => {
-    setNewFilter(ev.target.value);
-  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      filter shown with <input type="text" onChange={handleFilter} />
+      <Filter setF={setNewFilter}/>
       <h2>Add new contact:</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleName} />
-          <div>number: <input value={newPhone} onChange={handlePhone} /></div>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm persons={persons} newN={newName} newP={newPhone} setPers={setPersons} setN={setNewName} setP={setNewPhone} />
       <h2>Numbers</h2>
       <ul>
-        <DisplayNames persons={persons} filter={newFilter} />
+        <DisplayNames persons={persons} filter={newFilter} setPers={setPersons} setP={setNewPhone} setN={setNewName} />
       </ul>
     </div>
   );
