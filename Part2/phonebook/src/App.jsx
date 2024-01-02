fee52c926868dc6c90b6f5e7e2c71d5a0e2886b8
Lorenzo Marotta <lz.marotta@gmail.com>
2024-01-02 22:43:26 +0100

    2.6

---
+++ Part2/phonebook/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (ev) =>{
+    ev.preventDefault()
+    const idName = newName.split(" ").slice(0,1)
+    const obj ={
+      name: newName,
+      id: idName[0]
+    }
+    setPersons(persons.concat(obj))
+    setNewName("")
+  }
+  const handleName = (ev) =>{
+    setNewName(ev.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(val =>  <li key={val.id}>{val.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
