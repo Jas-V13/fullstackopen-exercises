e81dc265affe59dc94d6550df74f18b63ea63987
Lorenzo Marotta <lz.marotta@gmail.com>
2024-01-02 23:54:55 +0100

    2.9

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+const DisplayNames = ({ persons, filter }) => {
+  return persons
+    .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+    .map(person => (
+      <li key={person.name}>{person.name} {person.phone}</li>
+    ));
+}
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      phone: '040-1234567' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newPhone, setNewPhone] = useState('')
+    { name: 'Arto Hellas', phone: '040-1234567' }
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
+  const [newFilter, setNewFilter] = useState('');
 
-  const addName = (ev) =>{
-    ev.preventDefault()
+  const addName = (ev) => {
+    ev.preventDefault();
     let checkName = false;
     for(const i of persons){
       if(i.name === newName) {
@@ -19,27 +27,34 @@ const App = () => {
     };
     if(checkName) return;
     const idName = newName.split(" ").slice(0,1)
-    const obj ={
+    const obj = {
       name: newName,
       phone: newPhone,
       id: idName[0]
-    }
-    setPersons(persons.concat(obj))
-    setNewName("")
-  }
-  const handleName = (ev) =>{
-    setNewName(ev.target.value)
-  }
-  const handlePhone = (ev) =>{
-    setNewPhone(ev.target.value)
-  }
+    };
+    setPersons(persons.concat(obj));
+    setNewName('');
+    setNewPhone('');
+  };
+
+  const handleName = (ev) => {
+    setNewName(ev.target.value);
+  };
+  const handlePhone = (ev) => {
+    setNewPhone(ev.target.value);
+  };
+  const handleFilter = (ev) => {
+    setNewFilter(ev.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input type="text" onChange={handleFilter} />
+      <h2>Add new contact:</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleName}/>
+          name: <input value={newName} onChange={handleName} />
           <div>number: <input value={newPhone} onChange={handlePhone} /></div>
         </div>
         <div>
@@ -48,10 +63,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(val =>  <li key={val.id}>{val.name} {val.phone}</li>)}
+        <DisplayNames persons={persons} filter={newFilter} />
       </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
