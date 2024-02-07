387cad2d70429e26cc8d3f7f2c0c987e96f9ea51
Lorenzo Marotta <lz.marotta@gmail.com>
2024-02-07 18:51:43 +0100

    3.2

--- Part3/phonebookbackend/index.js
+++ Part3/phonebookbackend/index.js
@@ -28,6 +28,24 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
 
+app.get('/info', (re, rp) => {
+  const entries = persons.length
+  const now = new Date()
+  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];  
+  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const dayName = dayNames[now.getDay()];
+  const monthName = monthNames[now.getMonth()];
+  const day = now.getDate();
+  const year = now.getFullYear();
+  const hours = now.getHours().toString().padStart(2, '0'); // Ensure two digits
+  const minutes = now.getMinutes().toString().padStart(2, '0'); // Ensure two digits
+
+  const formattedDate = `${dayName} ${monthName} ${day} ${year} ${hours}:${minutes}`;
+  rp.send(
+    `<div><div>Phonebook has info for ${entries} people </div><div>${formattedDate}</div></div>`  
+  )
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
